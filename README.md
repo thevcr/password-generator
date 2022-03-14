0943a220beae1e1406a0c2b12eb94d68d22386cf
thevcr <thevcr@gmail.com>
2022-03-13 21:31:40 -0400

    add array concatenation

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Password Generator Starter Code
+# Password Generator
+
+## Purpose
+This is a password generator web application built with HTML, CSS, and JavaScript. The user is presented with a series of window prompts of which values they want (special, uppercase/lowercase, and numeric characters) to include in the password. Once the options have been selected, the password is generated to the webpage.
+
+![Password Generator](assets/images/password-generator-screenshot.png)
+
+## Built With
+* HTML
+* CSS
+* JavaScript
+
+## Website
+https://thevcr.github.io/password-generator
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,49 @@
 // Assignment code here
-var specialChar = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-var uppercaseChar = "";
-var lowercaseChar = "";
-var randomNumber = function(min,max) {
-var value = Math.floor(Math.random() * (max - min + 1) + min);
+var specialChar =  " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~".split("");
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz".split("");
+var numericChar = "1234567890".split("");
 
-return value;
-}
-
-
+var finalPassword = "";
 var generatePassword = function() {
   
+  // Prompt user for password length
   var passwordLength = window.prompt("What is your desired password length? Enter a value between 8 and 128.");
   if (passwordLength >= 8 && passwordLength <= 128) {
   
-    var confirmSpecialChar = window.confirm("Click OK button to add special characters, otherwise click CANCEL.");
-    var confirmUppercaseChar = window.confirm("Click OK button to add uppercase characters, otherwise click CANCEL.");
-    var confirmLowercaseChar = window.confirm("Click OK button to add lowercase characters, otherwise click CANCEL.");
-    var confirmNumericChar = window.confirm("Click OK button to add numeric characters, otherwise click CANCEL.");
+    // Prompt user for all available options and have them confirm what values they want in their password
+    var confirmSpecialChar = window.confirm("Click OK to add special characters, otherwise click CANCEL.");
+    var confirmUppercaseChar = window.confirm("Click OK to add uppercase characters, otherwise click CANCEL.");
+    var confirmLowercaseChar = window.confirm("Click OK to add lowercase characters, otherwise click CANCEL.");
+    var confirmNumericChar = window.confirm("Click OK to add numeric characters, otherwise click CANCEL.");
 
-    if (confirmSpecialChar) {
-      console.log("special char")
-    } if (confirmUppercaseChar) {
-      console.log("uppercase")
-    } if (confirmLowercaseChar) {
-      console.log("lowercase")
-    } if (confirmNumericChar) {
-      console.log("numeric")
-    }
-  }
-  else { 
+      // Add characters based on values user has chosen
+      if (confirmSpecialChar) {
+        finalPassword = finalPassword.concat(specialChar);
+        console.log(finalPassword);
+      } if (confirmUppercaseChar) {
+        finalPassword = finalPassword.concat(uppercaseChar);
+        console.log(finalPassword)
+      } if (confirmLowercaseChar) {
+        finalPassword = finalPassword.concat(lowercaseChar);
+        console.log(finalPassword)
+      } if (confirmNumericChar) {
+        finalPassword = pasfinalPassword.concat(numericChar);
+        console.log(finalPassword)
+      } if (!confirmSpecialChar && !confirmUppercaseChar && !confirmLowercaseChar && !confirmNumericChar) {
+        window.alert("Must select at least one character option")
+        return writePassword();
+      }
+      // for (var i = 0; i < passwordLength.length; i++) {
+      //   password += []
+      // }
+    } else { 
     window.alert("Password length must be between 8 and 128 characters.");
-    
+    return writePassword();
   }
-  //return generatePassword;
 };
 
-  
-
+var randomNumber = Math.floor(Math.random() * finalPassword.length);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -48,7 +54,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
